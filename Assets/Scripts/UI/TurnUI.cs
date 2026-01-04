153aee0935e9389117ed7651b04fbe78d4d2a108
uri-git23 <uryuuri21@gmail.com>
2026-01-04 12:27:39 +0900

    Feat: TurnTest용 씬 구현
    타이머 기능 Fix 필요

--- Assets/Scripts/UI/TurnUI.cs
+++ Assets/Scripts/UI/TurnUI.cs
@@ -1,22 +1,65 @@
 using UnityEngine;
 using TMPro;
+using Unity.Netcode;
+using System.Collections;
 
 public class TurnUI : MonoBehaviour
 {
+    [SerializeField] private GameObject turnPanel;
     [SerializeField] private TMP_Text turnText;
     [SerializeField] private TMP_Text timerText;
 
-    void Update()
+
+    void Start()
     {
-        // NullReferenceException 방지용
+        // 시작할 때는 팝업 패널 숨김
+        if(turnPanel != null) turnPanel.SetActive(false);
+    }
+
+    // TurnManager -> TurnUI로 신호 전달
+    private void OnEnable()
+    {
+        if(TurnManager.Instance == null) return; // NullReferenceException 방지
+
+        TurnManager.Instance.OnTurnChanged += HandleTurnChanged;
+    }
+
+    private void OnDisable() // 비활성화
+    {
         if(TurnManager.Instance == null) return;
 
-        // 현재 턴이 나인지
-        bool myTurn = TurnManager.Instance.IsMyTurn();
+        TurnManager.Instance.OnTurnChanged -= HandleTurnChanged; // 중복 호출 방지
+    }
 
-        turnText.text = myTurn ? "your turn" : "enemy's turn";
+    void Update()
+    {
+        if(TurnManager.Instance == null) return;
 
-        float time = TurnManager.Instance.GetRemainingTime();
-        timerText.text = Mathf.Ceil(time).ToString(); // 올림해서 정수 처리
+        // 타이머 갱신 (올림해서 정수 처리)
+        timerText.text = 
+            Mathf.Ceil(TurnManager.Instance.GetRemainingTime()).ToString();
+    }
+
+    // ID 비교해서 턴 판정
+    void HandleTurnChanged(ulong newTurnClientId)
+    {
+        bool IsMyTurn = 
+            NetworkManager.Singleton.LocalClientId == newTurnClientId;
+        
+        StopAllCoroutines();
+        StartCoroutine(ShowTurnPopup(IsMyTurn));
+    }
+
+    IEnumerator ShowTurnPopup(bool IsMyTurn)
+    {
+        turnText.text = IsMyTurn ? "your turn" : "enemy's turn";
+        turnText.color = IsMyTurn ? UnityEngine.Color.green : UnityEngine.Color.red;
+
+        turnPanel.SetActive(true);
+
+        // 2초 대기
+        yield return new WaitForSeconds(2f);
+
+        turnPanel.SetActive(false);
     }
 }
