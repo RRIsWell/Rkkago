c02346a8684e721da770e599cb9bfd19adce02c9
uri-git23 <uryuuri21@gmail.com>
2026-01-12 18:03:23 +0900

    Feat: 맵 기본 규칙 정의

---
+++ Assets/ScriptableObjects/Map/MapConfig.cs
@@ -0,0 +1,22 @@
+using System.Data.Common;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+[CreateAssetMenu(menuName = "Map/MapConfig")]
+public class MapConfig : ScriptableObject
+{
+    [Header("기본 규칙")]
+    public int stonesPerPlayer = 5;
+    public bool allowHalfOut = true;
+
+    [Header("표면")]
+    public float friction = 0.8f;
+    public bool slippery = false;
+
+    [Header("장애물")]
+    public bool useObstacle;
+
+    [Header("패배 판정")]
+    public int loseStoneCount = 5;
+}
