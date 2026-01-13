78ef98a7719e386c46c6fe0c0a93e8ea77e3db8a
uri-git23 <uryuuri21@gmail.com>
2026-01-13 09:33:44 +0900

    Feat: 맵의 경계 처리 스크립트

--- Assets/Scripts/Map/MapBoundary.cs
+++ Assets/Scripts/Map/MapBoundary.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class MapBoundary : MonoBehaviour
 {
+    float halfThreshold;
+
     public void Init(MapConfig config)
     {
-        // TODO: 나중에 더 구현
-        // killOutside, deathThresholdRatio 처리
+        halfThreshold = config.allowHalfOut ? 0.5f : 0f;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var stone = other.GetComponent<Stone>();
+        if (stone == null) return;
+
+        float outRatio = stone.GetOutRatio(); // 돌이 얼마나 밖으로 나갔는지
+        if (outRatio > halfThreshold)
+        {
+            stone.Kill();
+        }
     }
 }
