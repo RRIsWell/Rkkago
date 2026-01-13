d0ee4d9a90f00e776337167a2bf8e009615f04c3
uri-git23 <uryuuri21@gmail.com>
2026-01-13 09:49:41 +0900

    Feat: 장애물 스폰 설정 추가

--- Assets/Scripts/Map/ObstacleSpawner.cs
+++ Assets/Scripts/Map/ObstacleSpawner.cs
@@ -1,30 +1,41 @@
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    public GameObject[] obstaclePrefabs;
-    public BoxCollider2D spawnArea;
+    [Header("Prefabs")]
+    [SerializeField] private GameObject[] obstaclePrefabs;
+
+    [Header("Spawn Area")]
+    [SerializeField] private BoxCollider2D spawnArea;
 
     public void Init(MapConfig config)
     {
-        if (!config.useObstacle) return;
+        if (!config.useObstacle)
+            return;
 
-        int count = Random.Range(3, 7); // 나중에 밸런스패치로 조정
+        SpawnInternal(config);
+    }
+
+    void SpawnInternal(MapConfig config)
+    {
+        int count = config.obstacleCount;
+
         for (int i = 0; i < count; i++)
         {
-            Vector2 pos = GetRandomInside();
-            Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], pos, Quaternion.identity, transform);
+            Vector2 pos = GetValidRandomPosition(config.obstacleMargin);
+
+            GameObject prefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Instantiate(prefab, pos, Quaternion.identity, transform);
         }
     }
 
-    Vector2 GetRandomInside()
+    Vector2 GetValidRandomPosition(float margin)
     {
         Bounds b = spawnArea.bounds;
+
         return new Vector2(
-            Random.Range(b.min.x, b.max.x),
-            Random.Range(b.min.y, b.max.y)
+            Random.Range(b.min.x + margin, b.max.x - margin),
+            Random.Range(b.min.y + margin, b.max.y - margin)
         );
     }
 }
-
