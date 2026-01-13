827ad020977687eaeb07b9a458329093418c0645
uri-git23 <uryuuri21@gmail.com>
2026-01-13 09:36:01 +0900

    Feat: 맵의 표면 처리 스크립트

--- Assets/Scripts/Map/SurfaceController.cs
+++ Assets/Scripts/Map/SurfaceController.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class SurfaceController : MonoBehaviour
 {
+    Collider2D surface;
+
     public void Init(MapConfig config)
     {
-        // TODO: 나중에 더 구현
-        // frictionMultiplier 적용
+        surface = GetComponent<Collider2D>();
+
+        var mat = new PhysicsMaterial2D();
+        mat.friction = config.friction;
+        mat.bounciness = config.slippery ? 0.1f : 0f;
+
+        surface.sharedMaterial = mat;
     }
 }
