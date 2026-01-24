8dae07be38cd94fedbf18da244e8be97a39cb0d4
Lastgamer05 <kangda0519@gmail.com>
2026-01-24 21:40:17 +0900

    Feat: 무거워지는 스킬, 작아지는 스킬 구현

---
+++ Assets/Scripts/Stone/Skill/Hacking.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Hacking : SkillBase
+{
+    public override string SkillName => "Hacking";
+
+    public Hacking(Stone stone) : base(stone)
+    {
+    }
+
+    public override void Activate()
+    {
+    }
+}
