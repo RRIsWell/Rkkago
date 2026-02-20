665d48f6959c64ebdf4c7824a0f6008069c69440
Lastgamer05 <kangda0519@gmail.com>
2026-02-20 17:29:25 +0900

    Feat: lateBloomer 구현

---
+++ Assets/Scripts/Stone/Skill/SkillSO/LateBloomerSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LateBloomerSO", menuName = "Scriptable Objects/LateBloomerSO")]
+public class LateBloomerSO : SkillSO
+{
+    public float strength; 
+    public float debuffedWeight;
+    
+}
+
