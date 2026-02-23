3b1768abc8812029f581ae4eafc06530c0234790
uri-git23 <uryuuri21@gmail.com>
2026-02-24 04:44:48 +0900

    Feat: 동전 던지기 로직

--- Assets/Scripts/Gameplay/GameState.cs
+++ Assets/Scripts/Gameplay/GameState.cs
@@ -5,6 +5,7 @@ public enum GameState
     Waiting, // 플레이어 대기 중
     MatchIntro, // 매칭 화면
     SkillInfo, // 스킬 정보 표시
+    CoinFlip, // 선공 결정 동전 던지기
     Playing, // 실제 플레이
     SkillCutScene //TODO: 스킬 발동 화면
 }
